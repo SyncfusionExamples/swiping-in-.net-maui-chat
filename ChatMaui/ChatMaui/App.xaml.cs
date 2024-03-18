2c0dd786894b85a94bf62a50de0fec4c609f01b9
Lakshminatarajan <50870908+Lakshminatarajan@users.noreply.github.com>
2024-03-18 14:37:57 +0530

    Add files via upload

---
+++ ChatMaui/ChatMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ChatMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ ChatMaui/ChatMaui/ViewModels/ViewModel.cs
@@ -0,0 +1,129 @@
+﻿using Syncfusion.Maui.Chat;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatMaui
+{
+    internal class ViewModel:INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Collection of messages in a conversation.
+        /// </summary>
+        private ObservableCollection<object> messages;
+
+        /// <summary>
+        /// Current user of chat.
+        /// </summary>
+        private Author currentUser;
+
+        public ViewModel()
+        {
+            this.messages = new ObservableCollection<object>();
+            this.currentUser = new Author() { Name = "Nancy" };
+            this.GenerateMessages();
+        }
+
+        /// <summary>
+        /// Gets or sets the collection of messages of a conversation.
+        /// </summary>
+        public ObservableCollection<object> Messages
+        {
+            get
+            {
+                return this.messages;
+            }
+
+            set
+            {
+                this.messages = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the current user of the message.
+        /// </summary>
+        public Author CurrentUser
+        {
+            get
+            {
+                return this.currentUser;
+            }
+            set
+            {
+                this.currentUser = value;
+                RaisePropertyChanged("CurrentUser");
+            }
+        }
+
+
+        /// <summary>
+        /// Property changed handler.
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Occurs when property is changed.
+        /// </summary>
+        /// <param name="propName">changed property name</param>
+        public void RaisePropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        private void GenerateMessages()
+        {
+            this.messages.Add(new TextMessage()
+            {
+                Author = currentUser,
+                Text = "Hi guys, good morning! I'm very delighted to share with you the news that our team is going to launch a new mobile application.",
+            });
+
+            this.messages.Add(new TextMessage()
+            {
+                Author = new Author() { Name = "Andrea", Avatar = "Andrea.png" },
+                Text = "Oh! That's great.",
+            });
+
+            this.messages.Add(new TextMessage()
+            {
+                Author = new Author() { Name = "Harrison", Avatar = "Harrison.png" },
+                Text = "That is good news. Are we going to develop the app natively or hybrid,considering factors such as time constraints and development resources?"
+            });
+            this.messages.Add(new TextMessage()
+            {
+                Author = currentUser,
+                Text = "We should develop this app in .NET MAUI, since it provides native experience and performance",
+            });
+            this.messages.Add(new TextMessage()
+            {
+                Author = new Author() { Name = "Margaret", Avatar = "Margaret.png" },
+                Text = "I haven't heard of .NET MAUI. What's .NET MAUI?",
+            });
+            this.messages.Add(new TextMessage()
+            {
+                Author = currentUser,
+                Text = ".NET MAUI stands for .NET Multi-platform App UI. It's a framework for building native cross-platform mobile and desktop apps with .NET and C#. It allows you to write your code once and deploy it to multiple platforms, including Android, iOS, macOS, and Windows.",
+            });
+
+            this.messages.Add(new TextMessage()
+            {
+                Author = new Author() { Name = "Andrea", Avatar = "Andrea.png" },
+                Text = "That sounds interesting! How does it compare to other cross-platform frameworks like Xamarin.Forms?",
+            });
+
+            this.messages.Add(new TextMessage()
+            {
+                Author = currentUser,
+                Text = "Good question, Andrea! .NET MAUI builds upon the foundation of Xamarin.Forms but introduces several enhancements and improvements. It offers a more streamlined development experience with a single project structure, improved performance.",
+            });
+        }
+    }
+}
